f8fbe7ce80e5abf0af2dc590f20ddc26168b9ce6
Charlie <clarisse.vanlerberghe@gmail.com>
2024-12-24 11:39:45 +0100

    Continue d'ajout bubble-tea

--- app/data/bubble_table.sql
+++ app/data/bubble_table.sql
@@ -5,7 +5,8 @@ INSERT INTO "bar" ("name", "adresse", "horaire", "image1", "image2", "image3", "
 VALUES 
 ('Oshi-Moshi Plumereau', '3 rue du Change, 37000 TOURS', 'Lun au Dim : 14-21h', 'oshi-moshi-plume.png', NULL, NULL, NULL, NULL, NULL),
 ('Oshi-Moshi Gare', '25 rue Blazise Pascal, 37000 TOURS', 'Lun au Dim : 13-20h', 'oshi-moshi-gare.png', NULL, NULL, NULL, NULL, NULL),
-('Oshi-Moshi Angers', '3 rue des Lices, 49100 ANGERS', 'Lun au Sam : 12-19h Dim: 13h-19h', 'oshi-moshi-angers.png', NULL, NULL, NULL, NULL, NULL);
+('Oshi-Moshi Angers', '3 rue des Lices, 49100 ANGERS', 'Lun au Sam : 12-19h Dim: 13h-19h', 'oshi-moshi-angers.png', NULL, NULL, NULL, NULL, NULL)
+ON CONFLICT ("name") DO NOTHING;
 
 -- Insertion des données dans la table "tea"
 INSERT INTO "tea" ("name", "created_at", "updated_at")
@@ -31,7 +32,8 @@ VALUES
 ('Mojito (sans alcool)', CURRENT_TIMESTAMP, NULL),
 ('Rhum (sans alcool)', CURRENT_TIMESTAMP, NULL),
 ('Eau pétillante', CURRENT_TIMESTAMP, NULL),
-('Pêche', CURRENT_TIMESTAMP, NULL);
+('Pêche', CURRENT_TIMESTAMP, NULL)
+ON CONFLICT ("name") DO NOTHING;
 
 -- Insertion des données dans la table "milk"
 INSERT INTO "milk" ("name", "created_at", "updated_at")
@@ -46,7 +48,8 @@ VALUES
 ('Lait saveur crème brûlée', CURRENT_TIMESTAMP, NULL),
 ('Lait au chocolat saveur tiramisu', CURRENT_TIMESTAMP, NULL),
 ('Lait', CURRENT_TIMESTAMP, NULL),
-('Noix de coco', CURRENT_TIMESTAMP, NULL);
+('Noix de coco', CURRENT_TIMESTAMP, NULL)
+ON CONFLICT ("name") DO NOTHING;
 
 -- Insertion des données dans la table "pearl"
 INSERT INTO "pearl" ("name", "created_at", "updated_at")
@@ -59,7 +62,8 @@ VALUES
 ('Tapioca', CURRENT_TIMESTAMP, NULL),
 ('Citron vert', CURRENT_TIMESTAMP, NULL),
 ('Mangue', CURRENT_TIMESTAMP, NULL),
-('Ananas', CURRENT_TIMESTAMP, NULL);
+('Ananas', CURRENT_TIMESTAMP, NULL)
+ON CONFLICT ("name") DO NOTHING;
 
 -- Insertion des données dans la table "topping"
 INSERT INTO "topping" ("name", "created_at", "updated_at")
@@ -79,7 +83,12 @@ VALUES
 ('Mousse onctueuse matcha', CURRENT_TIMESTAMP, NULL),
 ('Sirop de sucre brun', CURRENT_TIMESTAMP, NULL),
 ('Caramel', CURRENT_TIMESTAMP, NULL),
-('Chocolat', CURRENT_TIMESTAMP, NULL);
+('Gingembre', CURRENT_TIMESTAMP, NULL),
+('Cardamone', CURRENT_TIMESTAMP, NULL),
+('Citron', CURRENT_TIMESTAMP, NULL),
+('Fleur de sureau', CURRENT_TIMESTAMP, NULL),
+('Chocolat', CURRENT_TIMESTAMP, NULL)
+ON CONFLICT ("name") DO NOTHING;
 
 -- Insertion des données dans la table "bubble_tea"
 INSERT INTO "bubble_tea" ("name", "prixL", "prixXL", "image", "icone", "collection_id", "milk_id", "created_at", "updated_at")
@@ -93,7 +102,9 @@ VALUES
 ('Athéna', NULL, 8.50, NULL, NULL, NULL, NULL, CURRENT_TIMESTAMP, NULL),
 ('Poséidon', NULL, 8.50, NULL, NULL, NULL, NULL, CURRENT_TIMESTAMP, NULL),
 ('Arès', NULL, 8.50, NULL, NULL, NULL, NULL, CURRENT_TIMESTAMP, NULL),
-('Hadès', NULL, 8.50, NULL, NULL, NULL, NULL, CURRENT_TIMESTAMP, NULL);
+('Hadès', NULL, 8.50, NULL, NULL, NULL, NULL, CURRENT_TIMESTAMP, NULL),
+('Akatsuki', 7.90, 8.90, NULL, NULL, NULL, NULL, CURRENT_TIMESTAMP, NULL)
+ON CONFLICT ("name") DO NOTHING;
 
 -- Insertion des relations multiples dans la table de jonction "bubble_tea_tea"
 INSERT INTO "bubble_tea_tea" ("bubble_tea_id", "tea_id") VALUES
@@ -102,24 +113,19 @@ INSERT INTO "bubble_tea_tea" ("bubble_tea_id", "tea_id") VALUES
 ((SELECT id FROM "bubble_tea" WHERE name = 'Quartz Rose'), (SELECT id FROM "tea" WHERE name = 'Rose')),
 ((SELECT id FROM "bubble_tea" WHERE name = 'Quartz Rose'), (SELECT id FROM "tea" WHERE name = 'Cerise')),
 
-
 -- Bubble Tea Améthyste
 ((SELECT id FROM "bubble_tea" WHERE name = 'Améthyste'), (SELECT id FROM "tea" WHERE name = 'Violette')),
 ((SELECT id FROM "bubble_tea" WHERE name = 'Améthyste'), (SELECT id FROM "tea" WHERE name = 'Litchi')),
 ((SELECT id FROM "bubble_tea" WHERE name = 'Améthyste'), (SELECT id FROM "tea" WHERE name = 'Myrtille')),
 
-
 -- Bubble Tea Bloody Mary
-((SELECT id FROM "bubble_tea" WHERE name = 'Bloody Mary'), (SELECT id FROM "tea" WHERE name = 'Violette')),
-((SELECT id FROM "bubble_tea" WHERE name = 'Bloody Mary'), (SELECT id FROM "tea" WHERE name = 'Litchi')),
-((SELECT id FROM "bubble_tea" WHERE name = 'Bloody Mary'), (SELECT id FROM "tea" WHERE name = 'Myrtille')),
-
+((SELECT id FROM "bubble_tea" WHERE name = 'Bloody Mary'), (SELECT id FROM "tea" WHERE name = 'Rose')),
+((SELECT id FROM "bubble_tea" WHERE name = 'Bloody Mary'), (SELECT id FROM "tea" WHERE name = 'Framboise')),
 
 -- Bubble Tea Bubble Tea Licorne
 ((SELECT id FROM "bubble_tea" WHERE name = 'Bubble Tea Licorne'), (SELECT id FROM "tea" WHERE name = 'Cerise')),
 ((SELECT id FROM "bubble_tea" WHERE name = 'Bubble Tea Licorne'), (SELECT id FROM "tea" WHERE name = 'Thé bleu')),
 ((SELECT id FROM "bubble_tea" WHERE name = 'Bubble Tea Licorne'), (SELECT id FROM "tea" WHERE name = 'Citron vert')),
-
 
 -- Bubble Tea Bubble Tea Sirène
 ((SELECT id FROM "bubble_tea" WHERE name = 'Bubble Tea Sirène'), (SELECT id FROM "tea" WHERE name = 'Mangue')),
@@ -127,11 +133,23 @@ INSERT INTO "bubble_tea_tea" ("bubble_tea_id", "tea_id") VALUES
 ((SELECT id FROM "bubble_tea" WHERE name = 'Bubble Tea Sirène'), (SELECT id FROM "tea" WHERE name = 'Citron vert')),
 
 -- Bubble Tea Zeus
-((SELECT id FROM "bubble_tea" WHERE name = 'Bubble Tea Sirène'), (SELECT id FROM "tea" WHERE name = 'Thé noir')),
-((SELECT id FROM "bubble_tea" WHERE name = 'Bubble Tea Sirène'), (SELECT id FROM "tea" WHERE name = 'Thé noir')),
-((SELECT id FROM "bubble_tea" WHERE name = 'Bubble Tea Sirène'), (SELECT id FROM "tea" WHERE name = 'Thé noir')),
-((SELECT id FROM "bubble_tea" WHERE name = 'Bubble Tea Sirène'), (SELECT id FROM "tea" WHERE name = 'Thé noir')),
-((SELECT id FROM "bubble_tea" WHERE name = 'Bubble Tea Sirène'), (SELECT id FROM "tea" WHERE name = 'Thé noir')),
+((SELECT id FROM "bubble_tea" WHERE name = 'Zeus'), (SELECT id FROM "tea" WHERE name = 'Mangue')),
 
+-- Bubble Tea Athena
+((SELECT id FROM "bubble_tea" WHERE name = 'Athéna'), (SELECT id FROM "tea" WHERE name = 'Pêche')),
+
+-- Bubble Tea Poseidon
+((SELECT id FROM "bubble_tea" WHERE name = 'Poséidon'), (SELECT id FROM "tea" WHERE name = 'Curaçao (sans alcool)')),
+
+-- Bubble Tea Arès
+((SELECT id FROM "bubble_tea" WHERE name = 'Arès'), (SELECT id FROM "tea" WHERE name = 'Framboise')),
+
+-- Bubble Tea Hadès
+((SELECT id FROM "bubble_tea" WHERE name = 'Hadès'), (SELECT id FROM "tea" WHERE name = 'Fraise')),
+((SELECT id FROM "bubble_tea" WHERE name = 'Hadès'), (SELECT id FROM "tea" WHERE name = 'Basilic')),
+
+-- Bubble Tea Akatsuki
+((SELECT id FROM "bubble_tea" WHERE name = 'Akatsuki'), (SELECT id FROM "tea" WHERE name = 'Fraise')),
+((SELECT id FROM "bubble_tea" WHERE name = 'Akatsuki'), (SELECT id FROM "tea" WHERE name = 'Passion')),
 
 COMMIT;
