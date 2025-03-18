113080fe403c43af8f95b785988ec43031173cb4
Charlie <joe.van868@gmail.com>
2025-03-18 13:01:27 +0100

    preparation au deploiement

--- config/config.js
+++ config/config.js
@@ -6,5 +6,11 @@ export default {
   development: {
     url: process.env.MYSQL_URL,
     dialect: 'mysql',
+    logging: false,
+  },
+  production: {
+    url: process.env.MYSQL_URL, // Render te donnera une nouvelle URL après le déploiement
+    dialect: 'mysql',
+    logging: false,
   },
 };
