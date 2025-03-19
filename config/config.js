b033572be36560f5081cfc35f46a144d6796c0ba
Charlie <joe.van868@gmail.com>
2025-03-19 10:08:18 +0100

    Modification du .env pour le rendre utilisable pas le client.js du dossier models

--- config/config.js
+++ config/config.js
@@ -6,11 +6,5 @@ export default {
   development: {
     url: process.env.MYSQL_URL,
     dialect: 'mysql',
-    logging: false,
-  },
-  production: {
-    url: process.env.MYSQL_URL, // Render te donnera une nouvelle URL après le déploiement
-    dialect: 'mysql',
-    logging: false,
   },
 };
