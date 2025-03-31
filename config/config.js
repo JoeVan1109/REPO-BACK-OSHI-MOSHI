d968885db79223e59931601523e929f7875ff7e2
Charlie <joe.van868@gmail.com>
2025-03-31 13:55:39 +0200

    add log

--- config/config.js
+++ config/config.js
@@ -2,6 +2,8 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+console.log('MYSQL_URL:', process.env.MYSQL_URL);  // Vérifier la valeur de MYSQL_URL dans les logs
+
 export default {
   development: {
     url: process.env.MYSQL_URL,
